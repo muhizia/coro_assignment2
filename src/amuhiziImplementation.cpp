5207425ec088f40b80058afb074541ea69e4c8fb
Muhizi Aristide <muhizia4@gmail.com>
2021-02-27 22:57:09 +0200

    Checking values for starting x

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -78,10 +78,10 @@ void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_
     geometry_msgs::Twist _msg;
     srand(time(NULL));
     while (ros::ok()){
-        dx = x_g ;//– current_x;
-        dy = y_g ;//– current_y;
+        dx = (x_g–current_x);
+        dy = (y_g-current_y);
         erro_pos = sqrt(dx*dx + dy*dy);
-        erro_h = atan2 (dy, dx) ;//– theta_g;
+        erro_h = atan2 (dy, dx)-theta_g;
 
 
         if (abs(erro_h) > erro_pos){
