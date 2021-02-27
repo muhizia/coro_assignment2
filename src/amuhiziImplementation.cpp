287ec892b939ff74e56e3c2438c44ee7ac69b1ae
Muhizi Aristide <muhizia4@gmail.com>
2021-02-27 08:13:13 +0200

    removing doubled function

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -104,7 +104,7 @@ void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_
             _msg.angular.z = 0;
         }
       //   ROS_INFO("Moving Linear.x = %.2f, angular.z %.2f\n", _msg.linear.x, _msg.angular.x);
-        ROS_INFO("Moving Linear.x = %.2f, angular.z %.2f\n", current_x, current_y);
+        ROS_INFO("Moving linear.x = %.2f, angular.z %.2f\n", x_g, x_g);
 
         pub.publish(_msg);
         ros::spinOnce();
