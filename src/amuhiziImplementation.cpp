4c22dea2f6c7fcbb0bf29adfc0bb60d87a18cf99
Muhizi Aristide <muhizia4@gmail.com>
2021-02-28 10:24:43 +0200

    changing float to double

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -67,47 +67,54 @@ void print_message_to_file(FILE *fp, char message[]) {
 }
 
 void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_g){
-    ros::Rate _rate(1);
-    int count = 0;
-    double dx, dy, currentX, currentY;
-    double erro_pos, erro_h;
-    geometry_msgs::Twist _msg;
-    srand(time(NULL));
-    while (ros::ok()){
-       currentX = double(current_x);
-       currentY = double(current_y);
-       ROS_INFO("Moving linear.x = %.2f, angular.z %.2f\n", currentX, currentY);
-        dx = x_g-currentX;
-        dy = y_g-currentY;
-        erro_pos = sqrt(dx*dx + dy*dy);
-        erro_h = atan2 (dy, dx)-theta_g;
+   ros::Rate _rate(1);
+   int count = 0;
+   double dx, dy, currentX, currentY;
+   double erro_pos, erro_h;
+   geometry_msgs::Twist _msg;
+   srand(time(NULL));
+
+   erro_pos = sqrt(dx*dx + dy*dy);
+   erro_h = atan2 (dy, dx)-theta_g;
+
+   while (erro_pos > 1e-2){
+      currentX = double(current_x);
+      currentY = double(current_y);
+      ROS_INFO("Moving linear.x = %.2f, angular.z %.2f\n", currentX, currentY);
+      dx = x_g-currentX;
+      dy = y_g-currentY;
+      erro_pos = sqrt(dx*dx + dy*dy);
+      erro_h = atan2 (dy, dx)-theta_g;
 
 
-        if (abs(erro_h) > erro_pos){
-            // set forward velocity: v = 0
-            // set angular velocity: w = Kph eh
-            _msg.linear.x = 0;
-            _msg.linear.y = 0;
-            _msg.linear.z = 0;
+      if (abs(erro_h)>1e-2){
+         // set forward velocity: v = 0
+         // set angular velocity: w = Kph eh
+         _msg.linear.x = 0;
+         _msg.linear.y = 0;
+         _msg.linear.z = 0;
 
-            _msg.angular.x = 0;
-            _msg.angular.y = 0;
-            _msg.angular.z = 2*3.14159*double(rand())/double(RAND_MAX)-1;
-        }else{
-            _msg.linear.x = double(rand())/double(RAND_MAX);;
-            _msg.linear.y = 0;
-            _msg.linear.z = 0;
+         _msg.angular.x = 0;
+         _msg.angular.y = 0;
+         _msg.angular.z = 2*3.14159*double(rand())/double(RAND_MAX)-1;
+      }else{
+         _msg.linear.x = double(rand())/double(RAND_MAX);;
+         _msg.linear.y = 0;
+         _msg.linear.z = 0;
 
-            _msg.angular.x = 0;
-            _msg.angular.y = 0;
-            _msg.angular.z = 0;
-        }
-         //   ROS_INFO("Moving Linear.x = %.2f, angular.z %.2f\n", _msg.linear.x, _msg.angular.x);
-        
+         _msg.angular.x = 0;
+         _msg.angular.y = 0;
+         _msg.angular.z = 0;
+      }
+      //   ROS_INFO("Moving Linear.x = %.2f, angular.z %.2f\n", _msg.linear.x, _msg.angular.x);
+      
 
-        pub.publish(_msg);
-        ros::spinOnce();
-        _rate.sleep();
-        count++;
-    }
+      _msg.linear.x = 0;
+      pub.publish(_msg);
+      ros::spinOnce();
+      _rate.sleep();
+      count++;
+   }
+
+   pub.publish(_msg);
 }
