11a33ade61329fb66d690dfa1628bfe356ade9f1
Muhizi Aristide <muhizia4@gmail.com>
2021-02-28 13:10:43 +0200

    changing float to double

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -91,7 +91,7 @@ void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_
       dx             = x_g-currentX;
       dy             = y_g-currentY;
       erro_pos       = sqrt(dx*dx + dy*dy);
-      erro_h         = atan2(dy, dx) - theta_start;
+      erro_h         = atan2(dy, dx) - currentTheta;
 
       ROS_INFO("Moving theta_g = %.2f, currentTheta %.2f\n", theta_g, currentTheta);
       ROS_INFO("Moving error pos = %.2f, erro header %.2f\n", erro_pos, erro_h);
