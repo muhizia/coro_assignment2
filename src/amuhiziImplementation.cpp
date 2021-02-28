b7f1fbf918383d150efeea6e7d5110d26976804f
Muhizi Aristide <muhizia4@gmail.com>
2021-02-28 10:37:59 +0200

    changing float to double

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -74,13 +74,18 @@ void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_
    geometry_msgs::Twist _msg;
    srand(time(NULL));
 
-   erro_pos = sqrt(dx*dx + dy*dy);
-   erro_h = atan2 (dy, dx)-theta_g;
+   erro_pos    = sqrt(dx*dx + dy*dy);
+   erro_h      = atan2 (dy, dx)-theta_g;
+   currentX    = current_x;
+   currentY    = current_y;
+   ROS_INFO("Moving error pos = %.2f, erro header %.2f\n", error_pos, error_h);
+   ROS_INFO("current x = %.2f, current y %.2f\n", currentX, currentY);
 
    while (erro_pos > 1e-2){
       currentX = double(current_x);
       currentY = double(current_y);
-      ROS_INFO("Moving linear.x = %.2f, angular.z %.2f\n", currentX, currentY);
+      ROS_INFO("Moving error pos = %.2f, erro header %.2f\n", error_pos, error_h);
+      ROS_INFO("current x = %.2f, current y %.2f\n", currentX, currentY);
       dx = x_g-currentX;
       dy = y_g-currentY;
       erro_pos = sqrt(dx*dx + dy*dy);
