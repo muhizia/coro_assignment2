afde444a837b16ee398d6bd0213ea4ff067d2ba3
Muhizi Aristide <muhizia4@gmail.com>
2021-03-02 14:24:54 +0200

    adding direction function

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -143,7 +143,7 @@ void MeMo(ros::Publisher  pub, double x_g, double y_g, double theta_g){
    geometry_msgs::Twist _msg;
    srand(time(NULL));
    bool is_start = true;
-   double Kpp = 0.05;
+   double Kpp = 0.1;
    double Kph = 0.5;
    ROS_INFO("Moving error pos = %.2f, erro header %.2f\n", erro_pos, erro_h);
    ROS_INFO("current x = %.2f, current y %.2f\n", currentX, currentY);
