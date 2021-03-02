217bb6a57c430d8daf9cc0a54b4b894179f23175
Muhizi Aristide <muhizia4@gmail.com>
2021-03-02 10:43:13 +0200

    adding direction function

--- src/amuhiziImplementation.cpp
+++ src/amuhiziImplementation.cpp
@@ -93,7 +93,7 @@ void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_
       erro_pos       = sqrt(dx*dx + dy*dy);
       erro_h         = atan2(dy, dx) - currentTheta;
 
-      ROS_INFO("Moving theta_g = %.2f, currentTheta %.2f\n", theta_g, currentTheta);
+      // ROS_INFO("Moving theta_g = %.2f, currentTheta %.2f\n", theta_g, currentTheta);
       // ROS_INFO("Moving error pos = %.2f, erro header %.2f\n", erro_pos, erro_h);
       // ROS_INFO("current Dx = %.2f, Dy %.2f\n", dx, dy);
       // ROS_INFO("current x = %.2f, current y %.2f\n", currentX, currentY);
@@ -108,8 +108,8 @@ void devideAndConquer(ros::Publisher  pub, double x_g, double y_g, double theta_
 
          _msg.angular.x = 0;
          _msg.angular.y = 0;
-         _msg.angular.z = Kph*erro_h;
-         // else _msg.angular.z = -Kph*erro_h;
+         if(direction) _msg.angular.z = Kph*erro_h;
+         else _msg.angular.z = -Kph*erro_h;
          ROS_INFO("angular z = %.2f error h = %.2f\n", Kph*erro_h, erro_h);
          ROS_INFO("============== Turning =========");
       }else{
